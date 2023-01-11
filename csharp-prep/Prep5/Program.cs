c76f9f888466b9d50548eaea1c61f2405df3df3d
SmawCity <107061521+SmawCity@users.noreply.github.com>
2023-01-11 13:26:31 -0700

    Add files via upload

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magic_number)
+        {
+            Console.Write("Guess the magic number:");
+
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == magic_number)
+            {
+                Console.WriteLine("Correct");
+            }
+        }
     }
 }
