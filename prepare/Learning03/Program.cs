e9d032ce46a74dffbb77b2cc0bec5d8db60502e7
SmawCity <107061521+SmawCity@users.noreply.github.com>
2023-02-06 22:59:46 -0700

    Add files via upload

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFraction());
+        Console.WriteLine(f1.GetDecimal());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFraction());
+        Console.WriteLine(f2.GetDecimal());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFraction());
+        Console.WriteLine(f3.GetDecimal());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFraction());
+        Console.WriteLine(f4.GetDecimal());
     }
 }
